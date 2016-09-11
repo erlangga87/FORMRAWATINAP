8c518935764021886435349ff2661e29c6c91f39
deva erlangga <deva.erlangga87@gmail.com>
2016-09-11 20:37:15 +0700

    Hasil akhir Tugas

--- app/src/main/java/id/sch/smktelkom_mlg/xirpl3_20/formrawatinap/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/xirpl3_20/formrawatinap/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
@@ -20,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     Button bOk;
     RadioButton rb1, rb2, rb3;
     TextView tvHasil;
+    CheckBox cb1, cb2, cb3;
     String[][] arNK = {{"Anggrek1", "Anggrek2", "Anggrek3", "Anggrek4", "Anggrek5"},
             {"Mawar1", "Mawar2", "Mawar3", "Mawar4", "Mawar5"},
             {"Cempaka1", "Cempaka2", "Cempaka3", "Cempaka4", "Cempaka5"}};
@@ -30,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cb1 = (CheckBox) findViewById(R.id.checkBoxB);
+        cb2 = (CheckBox) findViewById(R.id.checkBoxA);
+        cb3 = (CheckBox) findViewById(R.id.checkBoxD);
         rb1 = (RadioButton) findViewById(R.id.radioButton);
         rb2 = (RadioButton) findViewById(R.id.radioButton2);
         rb3 = (RadioButton) findViewById(R.id.radioButton3);
@@ -40,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         spK = (Spinner) findViewById(R.id.spinnerKamar);
         spNK = (Spinner) findViewById(R.id.spinnerNamaKamar);
 
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, listKota);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, listKota);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spNK.setAdapter(adapter);
 
@@ -72,41 +77,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void doClick() {
-        //  String Nama = etNama.getText().toString();
-        // String Penyakit = etPenyakit.getText().toString();
-        //  String kamar = spK.getSelectedItem().toString();
-        //  String nkamar =spNK.getSelectedItem().toString();
-        //   String hasil = null;
-        //    int Bayar;
-        //    if (rb1.isChecked())
-        //   {
-        //       hasil=rb1.getText().toString();
-        //      Bayar = 100000;
-        //   }
-        //   else if (rb2.isChecked())
-        //   {
-        //       hasil = rb2.getText().toString();
-        //      Bayar = 200000;
-        //    }
-        //    else if (rb3.isChecked())
-        //    {
-        //       hasil = rb3.getText().toString();
-        //      Bayar = 500000;
-        //  }
-        //  if (Nama.isEmpty())
-        //  {
-        //      etNama.setError("Nama Belum Diisi");
-        //   }
-        //    if (Penyakit.isEmpty())
-        //   {
-        //      etPenyakit.setError("Penyakit Belum Diisi");
-        //  }
-        // if (hasil == null)
-        //   {
-        //       tvHasil.setText("Anda Belum Menentukan Lamanya Inap");
-        //   }
 
-        // tvHasil.setText("******************RS Sehat Selalu******************"+"\n nama :"+Nama+"\n Anda Menderita Penyakit :"+Penyakit+"\n ");
+        String Nama = etNama.getText().toString();
+        String Penyakit = etPenyakit.getText().toString();
+        String kamar = spK.getSelectedItem().toString();
+        String nkamar = spNK.getSelectedItem().toString();
+        String hasil = null;
+        String cek = null;
+        int Bayar = 0;
+        if (rb1.isChecked()) {
+            hasil = rb1.getText().toString();
+            Bayar = 100000;
+        } else if (rb2.isChecked()) {
+            hasil = rb2.getText().toString();
+            Bayar = 200000;
+        } else if (rb3.isChecked()) {
+            hasil = "Lebih Dari 2 Hari";
+            Bayar = 500000;
+        }
+        if (cb1.isChecked()) {
+            cek = cb1.getText().toString();
+        }
+        if (cb2.isChecked()) {
+            cek = cb2.getText().toString();
+        }
+        if (cb3.isChecked()) {
+            cek = cb3.getText().toString();
+        }
+
+        if (Nama.isEmpty()) {
+            etNama.setError("Nama Belum Diisi");
+        }
+        if (Penyakit.isEmpty()) {
+            etPenyakit.setError("Penyakit Belum Diisi");
+        }
+
+
+        tvHasil.setText("************RS Sehat Selalu************" + "\n Nama :" + Nama + "\n Anda Menderita Penyakit :" + Penyakit + "\n Anda Akan Diarawat Di Kamar : " + nkamar + "\n Anda Akan Dirawat : " + hasil + "\n Anda Mendapatkan Perawatan Khusus : " + cek + "\n Biaya Yang Harus Dibayar Rp." + Bayar);
     }
 
 
